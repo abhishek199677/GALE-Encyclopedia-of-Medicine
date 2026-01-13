0450a637a36b9169352262c6e7cddbc4a224e71c
abhishek199677 <puttapoguabhishek1007@gmail.com>
2026-01-13 16:20:01 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="abhishek199677",
+    author_email="puttapoguabhishek1007@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
