c4e81eac343e11561b40424a375da8ec5962e1ba
abhishek199677 <puttapoguabhishek1007@gmail.com>
2026-01-27 17:11:08 +0530

    added helper file

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,7 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from langchain_huggingface import HuggingFaceEmbeddings
 from typing import List
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 
 
